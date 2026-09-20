11246a756c2ac0d5674980e24182385b737e908c
kruxbee <grind.karna.hai@gmail.com>
2026-09-20 22:43:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (80.86%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+        int sign = 1;
+
+        if (i < n && s[i] == '-') {
+            sign = -1;
+            i++;
+        }
+        else if (i < n && s[i] == '+') {
+            i++;
+        }
+        long long ans = 0;
+
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            ans = ans * 10 + digit;
+            if (sign == 1 && ans > INT_MAX) {
+                return INT_MAX;
+            }
+
+            if (sign == -1 && -ans < INT_MIN) {
+                return INT_MIN;
+            }
+
+            i++;
+        }
+
+        return (int)(sign * ans);
+    }
+};
