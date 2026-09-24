d0ebf870554d814bd4ccd83c6f2c188ad9ee2860
kruxbee <grind.karna.hai@gmail.com>
2026-09-24 22:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (37.19%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long MOD = 1000000007;
+
+    long long power(long long x, long long n) {
+        long long ans = 1;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans = (ans * x) % MOD;
+            }
+
+            x = (x * x) % MOD;
+            n = n / 2;
+        }
+
+        return ans;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long even = (n + 1) / 2;
+        long long odd = n / 2;
+
+        return (power(5, even) * power(4, odd)) % MOD;
+    }
+};
